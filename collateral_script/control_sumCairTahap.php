69e1f10264eb82711e38a1cd0f85524724742bc4
prabhaswara <prabhaswara@gmail.com>
2014-09-17 05:54:01 +0700

    pagi2

--- collateral_script/control_sumCairTahap.php
+++ collateral_script/control_sumCairTahap.php
@@ -5,4 +5,6 @@
  */
 ini_set('max_execution_time', 0);
 
+$sql="select lnc,count(tgl_cair_tahap_fondasi) from debitur where progress='BELUM SELESAI' and skim_pencairan='PARTIAL DROW DOWN' ".
+     "and skim_pks in('KAVLING BANGUN','INDENT') and tgl_cair_tahap_fondasi not in(null,'','0000-00-00')";
 ?>
